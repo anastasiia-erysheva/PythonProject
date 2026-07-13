6f512d56cf710466a02595f3920d165600d5f863
anastasiia-erysheva <erysh09@gmail.com>
2026-07-13 07:20:22 +0300

    feat: add solution for ChainMap_maps.py

--- Chain_Map_maps.py
+++ Chain_Map_maps.py
@@ -1,8 +1,8 @@
 from collections import ChainMap
 def get_all_values(chainmap, key):
     result = set()
-    chainmap_items = chainmap.maps
-    for d in chainmap_items:
+    chainmap_list = chainmap.maps
+    for d in chainmap_list:
         if key in d:
             result.add(d[key])
     return result
