9a126f8f8f38a88e6724f854e97f8df247ea4d2e
anastasiia-erysheva <erysh09@gmail.com>
2026-07-31 07:10:41 +0300

    feat: add solution for rec_memo.py

---
+++ rec_memo.py
@@ -0,0 +1,12 @@
+memo = {}
+def tribonacci(n):
+    if n <= 3:
+        return 1
+    if n in memo:
+        return memo[n]
+    result = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
+    memo[n] = result
+    return result
+
+
+print(tribonacci(45))
