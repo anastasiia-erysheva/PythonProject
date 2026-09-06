745d51b787aa65477b15e348adc27374c28611c8
anastasiia-erysheva <erysh09@gmail.com>
2026-09-06 15:18:52 +0300

    feat: add solution for func_number_sum.py

---
+++ stepik_tasks/func get_authenticator().py
@@ -0,0 +1,30 @@
+def get_authenticator():
+    attempts = 3
+    def auth(password):
+        nonlocal attempts
+        if attempts == 0:
+            return "Доступ блокирован"
+        if password == "python123":
+            return "Доступ разрешен"
+        else:
+            attempts -= 1
+            if attempts == 0:
+                return "Доступ блокирован"
+            else:
+                 return "Неверный пароль"
+    return auth
+login = get_authenticator()
+
+print(login("wrong_1"))
+print(login("wrong_2"))
+print(login("wrong_3"))
+print(login("python123"))
+
+
+
+
+
+
+
+
+
