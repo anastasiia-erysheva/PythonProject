86e0e377178e1f5c1b58fb7b73be9b8a7c45de5e
anastasiia-erysheva <erysh09@gmail.com>
2026-09-22 06:58:53 +0300

    feat: add solution for func_prefix.py

---
+++ func_prefix.py
@@ -0,0 +1,20 @@
+from functools import wraps
+def prefix(string, to_the_end = False):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if to_the_end:
+                return result + string
+            else:
+                return string + result
+        return wrapper
+    return decorator
+
+
+@prefix('$$$', to_the_end=True)
+def get_bonus():
+    return '2000'
+
+
+print(get_bonus())
